e0782d0c83063f499a9372a463493d399a768d92
reinhartlim <18221154@std.stei.itb.ac.id>
2023-11-19 22:47:54 +0700

    style: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,21 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      }, 
+      colors: {
+        green: {
+          400: '#DBE5CA',
+          500: '#9AA785',
+          600: '#616A51',
+        },
+        pink: {
+          400: '#F6D3D1',
+          500: '#FEB291',
+          600: '#B6503C',
+        },
+      }
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
 export default config
