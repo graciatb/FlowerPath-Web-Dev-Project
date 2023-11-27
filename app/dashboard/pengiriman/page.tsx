4d163a78a2a15c74dbce1a74dbc1d44a0b96bb25
reinhartlim <18221154@std.stei.itb.ac.id>
2023-11-27 23:27:25 +0700

    feat: add current user name to dashboard

--- app/dashboard/pengiriman/page.tsx
+++ app/dashboard/pengiriman/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import Search from "@/components/search";
 import PengirimanTable from "@/components/PengirimanTable";
 import Pagination from "@/components/pagination";
-import { fetchPengirimanPages } from "@/lib/action/data";
+import { fetchCurrentUserName, fetchPengirimanPages } from "@/lib/action/data";
 
 export default async function Page({
   searchParams,
@@ -23,37 +23,39 @@ export default async function Page({
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchPengirimanPages(query);
+  const nama = await fetchCurrentUserName();
   return (
     <div className="flex flex-col h-screen bg-slate-50">
       {/* Navbar */}
       <div className="p-7 pl-14 bg-gray-200">
-        <div className="flex justify-between items-center">
-          <div>Welcome, Michael Sihotang</div>
-          </div>
+        <div className="flex justify-left space-x-1">
+          <div>Welcome, </div>
+          <div className="font-bold">{nama}</div>
         </div>
-        <div
-          className="pt-7 pl-14 text-2xl text-orange-700 font-bold bg-center"
-        >
-          {/* Judulnya */}
-          <h1>Pengiriman</h1>
-        </div>
+      </div>
+      <div className="pt-7 pl-14 text-2xl text-orange-700 font-bold bg-center">
+        {/* Judulnya */}
+        <h1>Pengiriman</h1>
+      </div>
 
-        <div className="pl-14 pr-14 pt-8">
+      <div className="pl-14 pr-14 pt-8">
         <div className="relative overflow-x-auto bg-white w-full rounded-md">
-        <div className="pl-12 pt-8 px-8 grid grid-cols-2 gap-3">
-          {/* Untuk tombolnya */}
-          <Link 
-          href="/dashboard/pengiriman/tambahpengiriman"
-          className="flex justify-center w-full py-2 h-9 bg-green-400 bg-opacity-90 rounded-full hover:bg-green-500 text-sm text-green-600 font-semibold hover:text-white">
-            Tambah Pengiriman
-          </Link>
+          <div className="pl-12 pt-8 px-8 grid grid-cols-2 gap-3">
+            {/* Untuk tombolnya */}
+            <Link
+              href="/dashboard/pengiriman/tambahpengiriman"
+              className="flex justify-center w-full py-2 h-9 bg-green-400 bg-opacity-90 rounded-full hover:bg-green-500 text-sm text-green-600 font-semibold hover:text-white"
+            >
+              Tambah Pengiriman
+            </Link>
 
-          <Link
-          href="/dashboard/pengiriman"
-          className="flex justify-center w-full py-2 h-10 w-full bg-green-400 bg-opacity-90 rounded-full hover:bg-green-500 text-sm text-green-600 font-semibold hover:text-white ">
-            Daftar Pengiriman
-          </Link>
-        </div>
+            <Link
+              href="/dashboard/pengiriman"
+              className="flex justify-center py-2 h-10 w-full bg-green-400 bg-opacity-90 rounded-full hover:bg-green-500 text-sm text-green-600 font-semibold hover:text-white "
+            >
+              Daftar Pengiriman
+            </Link>
+          </div>
           <div className="ml-12 w-11/12 flex items-center justify-between gap-2 sm:mt-2 md:mt-3">
             {/* Set width to full width */}
             <Search placeholder="Search" />
@@ -67,11 +69,15 @@ export default async function Page({
             </Link>
           </div>
 
-        <div className="flex pt-3 pb-2 items-center justify-center md:px-0 w-full">
-            <PengirimanTable query={query} currentPage={currentPage} riwayat = {false} />
+          <div className="flex pt-3 pb-2 items-center justify-center md:px-0 w-full">
+            <PengirimanTable
+              query={query}
+              currentPage={currentPage}
+              riwayat={false}
+            />
           </div>
-          <div className="sm:pt-1 xl:pt-0 sm:pb-2 justify-center xl:pt-4 2xl:px-8 xl:pb-4 w-full flex">
-          <Pagination totalPages={totalPages} />
+          <div className="sm:pt-1 sm:pb-2 justify-center xl:pt-4 2xl:px-8 xl:pb-4 w-full flex">
+            <Pagination totalPages={totalPages} />
           </div>
         </div>
       </div>
--- app/kurir/page.tsx
+++ app/kurir/page.tsx
@@ -1,86 +1,84 @@
 import { BsBoxSeam } from "react-icons/bs";
 import { AiOutlineFileDone } from "react-icons/ai";
 import { IoWarningOutline } from "react-icons/io5";
-import { fetchDeliveredPaketCount, fetchOnHoldPaketCount, fetchPaketCount } from "@/lib/action/data";
+import {
+  fetchCurrentUserName,
+  fetchDeliveredPaketCount,
+  fetchOnHoldPaketCount,
+  fetchPaketCount,
+} from "@/lib/action/data";
 
 export default async function Page() {
-
   const countpaket = fetchPaketCount();
   const countdelivered = fetchDeliveredPaketCount();
   const countproblem = fetchOnHoldPaketCount();
-    return (
-        <div className="flex flex-col h-screen bg-slate-50">
+  const nama = fetchCurrentUserName();
+  return (
+    <div className="flex flex-col h-screen bg-slate-50">
+      {/* Set header */}
+      <div className="p-7 pl-14 bg-gray-200">
+        <div className="flex justify-left space-x-1">
+          <div>Welcome, </div>
+          <div className="font-bold">{nama}</div>
+        </div>
+      </div>
 
-            {/* Set header */}        
-            <div className="p-7 pl-14 bg-gray-200">
-                <div className="flex justify-left space-x-1">
-                    <div> Welcome,</div>
-                    <div className = "font-bold">
-                    <div> Michael Sihotang </div>
-                    </div>
-                </div>
-            </div>
+      {/* Set text */}
+      <div className="pt-16 pl-14 text-4xl text-orange-700 font-bold flex-grow bg-center p-0 m-0">
+        <div>Dashboard</div>
+      </div>
 
-            {/* Set text */}
-            <div className="pt-16 pl-14 text-4xl text-orange-700 font-bold flex-grow bg-center p-0 m-0">
-                <div>Dashboard</div>
-            </div>
-                
-            {/* Set white box */}
-            <div className="items-center justify-center py-8 px-12 h-full">
-                <div className="py-28 px-12 bg-white bg-opacity-90 rounded-xl overflow-hidden">
-                <div className="grid grid-cols md:grid-cols-3 gap-10">
-      
-              {/* Set first box */}
-              <div className="bg-green-400 w-100 pr-5 py-10 rounded-xl flex items-center bg-opacity-100">
-                <div className="pr-4 ml-7">
-                  <div className="text-lg font-medium text-gray-600">
-                    Total Paket Berlangsung
-                  </div>
-                  <div className="font-bold text-3xl">
-                  {countpaket}</div>
+      {/* Set white box */}
+      <div className="items-center justify-center py-8 px-12 h-full">
+        <div className="py-28 px-12 bg-white bg-opacity-90 rounded-xl overflow-hidden">
+          <div className="grid grid-cols md:grid-cols-3 gap-10">
+            {/* Set first box */}
+            <div className="bg-green-400 w-100 pr-5 py-10 rounded-xl flex items-center bg-opacity-100">
+              <div className="pr-4 ml-7">
+                <div className="text-lg font-medium text-gray-600">
+                  Total Paket Berlangsung
                 </div>
-                <div className="overflow-hidden">
+                <div className="font-bold text-3xl">{countpaket}</div>
+              </div>
+              <div className="overflow-hidden">
                 <div className="mr-1 py-5 px-5 bg-green-500 bg-opacity-100 rounded-full">
-                <BsBoxSeam color="white" size="38"/>
-                </div>
+                  <BsBoxSeam color="white" size="38" />
                 </div>
               </div>
+            </div>
 
-              {/* Set second box */}
-              <div className="bg-green-400 w-100 pr-5 py-10 rounded-xl flex items-center bg-opacity-100">
-                <div className="pr-4 ml-7">
-                  <div className="text-lg font-medium text-gray-600">
-                    Total Paket Selesai
-                  </div>
-                  <div className="font-bold text-3xl">
-                  {countdelivered}</div>
+            {/* Set second box */}
+            <div className="bg-green-400 w-100 pr-5 py-10 rounded-xl flex items-center bg-opacity-100">
+              <div className="pr-4 ml-7">
+                <div className="text-lg font-medium text-gray-600">
+                  Total Paket Selesai
                 </div>
-                <div className="overflow-hidden">
+                <div className="font-bold text-3xl">{countdelivered}</div>
+              </div>
+              <div className="overflow-hidden">
                 <div className="ml-12 py-4 px-4 bg-green-500 bg-opacity-100 rounded-full">
-                <AiOutlineFileDone color="white" size="40"/>
-                </div>
+                  <AiOutlineFileDone color="white" size="40" />
                 </div>
               </div>
+            </div>
 
-              {/* Set third box */}
-              <div className="bg-pink-400 w-100 pr-5 py-10 rounded-xl flex items-center bg-opacity-100">
-                <div className="pr-4 ml-7">
-                  <div className="text-lg font-medium text-gray-600">
-                    Total Paket Bermasalah
-                  </div>
-                  <div className="font-bold text-3xl">
-                  {countproblem}</div>
+            {/* Set third box */}
+            <div className="bg-pink-400 w-100 pr-5 py-10 rounded-xl flex items-center bg-opacity-100">
+              <div className="pr-4 ml-7">
+                <div className="text-lg font-medium text-gray-600">
+                  Total Paket Bermasalah
                 </div>
-                <div className="overflow-hidden">
-                <div className="ml-2 py-4 px-4 bg-pink-500 bg-opacity-100 rounded-full">
-                <IoWarningOutline color="white" size="40"/>
-                </div>
+                <div className="font-bold text-3xl">{countproblem}</div>
               </div>
+              <div className="overflow-hidden">
+                <div className="ml-2 py-4 px-4 bg-pink-500 bg-opacity-100 rounded-full">
+                  <IoWarningOutline color="white" size="40" />
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-};
+    </div>
+  );
+}
