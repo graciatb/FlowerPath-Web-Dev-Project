d49107e6b1d02609d6f5ebd4831fe608ec26d4c9
Gracia Theophilia <18221078@std.stei.itb.ac.id>
2023-11-27 15:23:01 +0000

    Dashboard frontend

--- app/dashboard/pengiriman/[id]/detail/page.tsx
+++ app/dashboard/pengiriman/[id]/detail/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Image from "next/image";
 import { fetchPengirimanById } from "@/lib/action/data";
 import { notFound } from "next/navigation";
+import { FaArrowLeft } from "react-icons/fa";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
@@ -20,48 +21,41 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   /*main kodenya*/
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-screen bg-slate-50">
       {/* Navbar */}
-      <div className="p-8 bg-gray-300 ">
+      <div className="p-7 pl-14 bg-gray-200">
+        <div className="flex justify-between items-center">
         <div className="flex justify-between items-center">
           <div>Welcome, Michael Sihotang</div>
-          <div>
-            <Image
-              src="/profil-image.png"
-              alt="User Profile"
-              className="w-15 h-15 rounded-full"
-              width={40}
-              height={40}
-            />
-          </div>
         </div>
       </div>
-
-      {/* Background */}
-      <div
-        className="flex-grow bg-cover bg-center p-0 m-0 "
-        style={{
-          backgroundImage: 'url("/bg.png")',
-          paddingTop: 0,
-          paddingRight: 0,
-          paddingBottom: 0,
-          paddingLeft: 0,
-        }}
-      >
+      </div>
         <a
           href="\dashboard\pengiriman"
-          className="text-orange-700 md:px-10 py-5 flex items-center"
-          style={{ fontSize: "24px", fontWeight: "bold" }}
+          className="flex pt-7 pl-14 text-2xl text-orange-700 font-bold bg-center"
         >
           {/* Judulnya */}
-          <ArrowLeftIcon className="w-4 md:w-6 mr-2" />{" "}
+          <div className="pt-1"> 
+          <FaArrowLeft color="pink-500" hover="bg-pink-200" />
+          </div>
+          <div className="pl-2"> 
           <span>Detail Pengiriman</span>
+          </div>
         </a>
+        <div className="pl-14 pr-14 pt-8">
+        <div className="relative overflow-x-auto bg-white w-full rounded-md">
+        <div className="flex items-center pt-6 px-8 pb-0">
+            <div className="rounded-md h-8 w-full bg-green-400 bg-opacity-90">
+              <div className="py-1 justify-center w-full flex text-md font-semibold text-green-600">
+                Pengiriman Paket
+              </div>
+            </div>
+          </div>
 
-        <div className="flex items-center justify-center md:p-10">
-          <div className="flex items-center p-5 bg-white rounded-md bg-opacity-70 w-full">
+        <div className="flex items-center justify-center md:p-5">
+          <div className="flex items-center px-5 sm:pt-5 md:py-0 bg-white rounded-md bg-opacity-70 w-full overflow-x-auto">
             <table className="w-full text-sm text-left text-gray-500">
-              <thead className="bg-orange-700 text-yellow-500">
+              <thead className="bg-orange-800 text-white">
                 <tr>
                   <th scope="col" className="py-4 px-6 border-b">
                     Nama Kurir
@@ -102,19 +96,19 @@ export default async function Page({ params }: { params: { id: string } }) {
               </tbody>
             </table>
           </div>
-        </div>
+          </div>
+          
+          <div className="sm:pt-5 px-8 pb-5">
+          <div className="rounded-md h-8 w-full bg-green-400 bg-opacity-90">
+              <div className="py-1 justify-center w-full flex text-md font-semibold text-green-600">
+                Keterangan Paket
+              </div>
+          </div>
+          </div>
 
-        <div className="flex items-center justify-center md:px-10">
-          <div className="flex items-center p-5 bg-white rounded-md bg-opacity-70 w-full">
-            <div
-              className="text-orange-700 md:px-10 py-5"
-              style={{ fontSize: "24px", fontWeight: "bold" }}
-            >
-              {/* Judulnya */}
-              <h1>Keterangan Paket</h1>
-            </div>
+          <div className="pb-10 px-10">
             <table className="w-full text-sm text-left text-gray-500">
-              <thead className="bg-orange-700 text-yellow-500">
+              <thead className="bg-orange-800 text-white">
                 <tr>
                   <th scope="col" className="py-4 px-6 border-b">
                     Tautan Gambar
@@ -130,13 +124,12 @@ export default async function Page({ params }: { params: { id: string } }) {
                         <tr className="bg-white">
                             <td className="py-4 px-6 border-b">{detail.fotopengiriman ? detail.fotopengiriman : "Tidak ada foto"}</td>
                             <td className="py-4 px-6 border-b">{detail.catatanpengiriman ? detail.catatanpengiriman : "Tidak ada catatan"}</td>
-                        </tr>
-                       
+                        </tr>          
               </tbody>
             </table>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
